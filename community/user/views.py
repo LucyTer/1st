2fd1fe6a7b9ab309d8fa05f315b69cf4aa3d3b53
pah0729 <pah0729@icloud.com>
2020-09-08 10:49:20 +0900

    user 수정

--- community/user/views.py
+++ community/user/views.py
@@ -1,7 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from .models import User
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        passwrod = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+
+        res_data = {}
+
+        if not (username and useremail and passwrod and re_password):
+            res_data['error'] = '모든 값을 입력해주세요!'
+        elif passwrod != re_password:
+            res_data['error'] = '비밀번호가 다릅니다!'
+        else:
+            user = User(
+                username=username,
+                useremail=useremail,
+                password=make_password(passwrod)
+            )
+
+            user.save()
+
+        return render(request, 'register.html', res_data)
